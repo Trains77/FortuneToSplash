225bf5ebb443a02a38578947ffd94876e63dbb7c
Trains77 <trains77videos123@gmail.com>
2022-03-26 20:15:38 -0400

    Added custom logic for BOFH excuses fortune

--- main.py
+++ main.py
@@ -18,10 +18,17 @@ if not os.path.exists("FortuneResourcePack/assets/minecraft/texts"):
     os.mkdir("FortuneResourcePack/assets/minecraft/texts")
 
 with open("FortuneResourcePack/pack.mcmeta", "a") as pack:
-    pack.writelines(["{\n", '    "pack_format": 8,\n', '    "description": "FortuneToSplashes Resource pack"\n', '  }\n', '}\n' ])
+    pack.writelines(["{\n", '    "pack_format": 8,\n', '    "description": "Fortune Resource pack"\n', '  }\n', '}\n' ])
 
-splashes = open("FortuneResourcePack/assets/minecraft/texts/splashes.txt", "w")
+splashes = open("splashes.txt", "w")
 splashes.write(str(line))
+
+with open('splashes.txt', 'r') as splash:
+    lines = splash.readlines()
+    with open('FortuneResourcePack/assets/minecraft/texts/splashes.txt', 'w') as fw:
+        for line in lines:
+            if line.find('BOFH excuse') == -1:
+                fw.write(line)
 splashes.close()
 
 print("Saved resource pack to folder FortuneResourcePack")
