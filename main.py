d97866e1a784bc759f87de04869498a169bd9286
Trains77 <trains77videos123@gmail.com>
2022-03-26 20:27:22 -0400

    Now saved as a zip file

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import os, sys
+import os, sys, shutil
+import zipfile
 
 ARGS = sys.argv
 FortunePath = open(str(ARGS[1]),"r")
 lines = FortunePath.read()
 line = str(lines)
-
+resource_pack_file_name = "FortuneResourcePack.zip"
 line = line.replace('%', ' ')
 
 
-if not os.path.exists("FortuneResourcePack"):
-    os.mkdir("FortuneResourcePack")
-if not os.path.exists("FortuneResourcePack/assets"):
-    os.mkdir("FortuneResourcePack/assets")
-if not os.path.exists("FortuneResourcePack/assets/minecraft"):
-    os.mkdir("FortuneResourcePack/assets/minecraft")
-if not os.path.exists("FortuneResourcePack/assets/minecraft/texts"):
-    os.mkdir("FortuneResourcePack/assets/minecraft/texts")
 
-with open("FortuneResourcePack/pack.mcmeta", "a") as pack:
+if not os.path.exists("assets"):
+    os.mkdir("assets")
+if not os.path.exists("assets/minecraft"):
+    os.mkdir("assets/minecraft")
+if not os.path.exists("assets/minecraft/texts"):
+    os.mkdir("assets/minecraft/texts")
+
+with open("pack.mcmeta", "a") as pack:
     pack.writelines(["{\n", '    "pack_format": 8,\n', '    "description": "Fortune Resource pack"\n', '  }\n', '}\n' ])
 
 splashes = open("splashes.txt", "w")
@@ -26,12 +26,18 @@ splashes.write(str(line))
 
 with open('splashes.txt', 'r') as splash:
     lines = splash.readlines()
-    with open('FortuneResourcePack/assets/minecraft/texts/splashes.txt', 'w') as fw:
+    with open('assets/minecraft/texts/splashes.txt', 'w') as fw:
         for line in lines:
             if line.find('BOFH excuse') == -1:
                 fw.write(line)
+
 splashes.close()
 
-os.remove("splashes.txt")
+with zipfile.ZipFile(resource_pack_file_name, 'a') as myzip:
+    myzip.write('assets/minecraft/texts/splashes.txt')
+    myzip.write('pack.mcmeta')
 
-print("Saved resource pack to folder FortuneResourcePack")
+os.remove("splashes.txt")
+shutil.rmtree("assets")
+os.remove("pack.mcmeta")
+print("Saved resource pack to folder FortuneResourcePack.zip")
