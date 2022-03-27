ff8b9db587b952ff61dcfcc6d30f48f7d7439eb8
Trains77 <trains77videos123@gmail.com>
2022-03-26 20:31:04 -0400

    Updated README

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Converts fortune files into a minecraft resource pack
 
-How to run: `python3 main.py PATH_TO_FORTUNE_FILE`
+How to run: `python main.py PATH_TO_FORTUNE_FILE`
 
 This script has been tested on Linux, but it should work on MacOS and Windows
 
-## What I hope to add:
+## What I plan to add:
 
 Multiple fortune files support
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, sys, shutil, zipfile
 
+# Variables
 ARGS = sys.argv
 FortunePath = open(str(ARGS[1]),"r")
 lines = FortunePath.read()
@@ -7,8 +8,7 @@ line = str(lines)
 resource_pack_file_name = "FortuneResourcePack.zip"
 line = line.replace('%', ' ')
 
-
-
+# Create needed paths
 if not os.path.exists("assets"):
     os.mkdir("assets")
 if not os.path.exists("assets/minecraft"):
@@ -16,27 +16,30 @@ if not os.path.exists("assets/minecraft"):
 if not os.path.exists("assets/minecraft/texts"):
     os.mkdir("assets/minecraft/texts")
 
+# Create pack.mcmeta file
 with open("pack.mcmeta", "a") as pack:
     pack.writelines(["{\n", '    "pack_format": 8,\n', '    "description": "Fortune Resource pack"\n', '  }\n', '}\n' ])
 
+# Create splashes.txt file
 splashes = open("splashes.txt", "w")
 splashes.write(str(line))
-
-
 with open('splashes.txt', 'r') as splash:
     lines = splash.readlines()
     with open('assets/minecraft/texts/splashes.txt', 'w') as fw:
         for line in lines:
             if line.find('BOFH excuse') == -1:
                 fw.write(line)
-
 splashes.close()
 
+# Put everything in a zip file
 with zipfile.ZipFile(resource_pack_file_name, 'a') as myzip:
     myzip.write('assets/minecraft/texts/splashes.txt')
     myzip.write('pack.mcmeta')
 
+# Cleanup files
 os.remove("splashes.txt")
 shutil.rmtree("assets")
 os.remove("pack.mcmeta")
+
+# Print
 print("Saved resource pack to folder FortuneResourcePack.zip")
